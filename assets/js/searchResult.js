f0497c6052eddc9da647002cfa4b36b0f25528c8
Nik Mirza <nik96mirza@gmail.com>
2017-10-15 00:31:53 +0800

    SearchResult Search Bar

--- assets/js/searchResult.js
+++ assets/js/searchResult.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
+    var el = tickerList.map(function(o){
+        return("<option value='"+o+"'>"+o+"</option>")
+    })
+    $("#search-data").append(el);
+
+    $("#search-bar").on("change",function(){
+        $("#search-btn").attr("href", "/searchResult.html?ticker="+$("#search-bar").val());
+    });
+    
     var query = querystring.parse();
     console.log(query);
 
