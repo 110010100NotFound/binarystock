e8863952490a7674ff72a0f73e455e0aed97516f
Nik Mirza <nik96mirza@gmail.com>
2017-10-14 21:59:12 +0800

    Changed the Scale of Pentagon

--- assets/js/searchResult.js
+++ assets/js/searchResult.js
@@ -19,7 +19,7 @@ $(document).ready(function(){
         scoresList[4] = data.scoreList["valueScores"];
 
         myChart.data.datasets[0].data = scoresList;
-        console.log(scoresList);
+        console.log(myChart);
         myChart.update();
     });
 
@@ -29,31 +29,18 @@ $(document).ready(function(){
             labels: ["Growth", "Liquidity", "Profitability", "Trend", "Value"],
             datasets: [{
                 label: 'Scores',
-                data: [0,0,0,0,0],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)'
-                ],
-                borderWidth: 1
+                data: [0,0,0,0,0]
             }]
         },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero:true
-                    }
-                }]
+        options:{
+            scale: {
+                display: true,
+                ticks: {
+                    beginAtZero: true,
+                    min: 0,
+                    max: 5.0,
+                    stepSize: 1.0
+                }
             }
         }
     });
